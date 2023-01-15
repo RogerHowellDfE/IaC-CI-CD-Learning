27e3944431a9ef31a78e5150e9714402c3ead690
Roger Howell <roger.howell@education.gov.uk>
2023-01-15 00:08:12 +0000

    Copy files from tutorial 03, and prep them for tutorial 04

---
+++ deploy/main-tutorial-04.bicep
@@ -0,0 +1,92 @@
+@description('The Azure region into which the resources should be deployed.')
+param location string = resourceGroup().location
+
+@description('The type of environment. This must be nonprod or prod.')
+@allowed([
+  'nonprod'
+  'prod'
+])
+param environmentType string
+
+@description('A unique suffix to add to resource names that need to be globally unique.')
+@maxLength(13)
+param resourceNameSuffix string = uniqueString(resourceGroup().id)
+
+var namePrefix = 'xyz-tutorial-04'
+var namePrefixAlphanumeric = 'xyztut04' // all lowercase alphanumeric, max 11 chars long (max 24chars when combined with 13char resource name suffix)
+
+var appServiceAppName = '${namePrefix}-${resourceNameSuffix}'
+var appServicePlanName = '${namePrefix}-plan'
+var xyzStorageAccountName = '${namePrefixAlphanumeric}${resourceNameSuffix}' // lowercase alphanumeric only, and 3-24 characters long
+ 
+// Define the SKUs for each component based on the environment type.
+var environmentConfigurationMap = {
+  nonprod: {
+    appServiceApp: {
+      alwaysOn: false
+    }
+    appServicePlan: {
+      sku: {
+        name: 'F1'
+        capacity: 1
+      }
+    }
+    xyzStorageAccount: {
+      sku: {
+        name: 'Standard_LRS'
+      }
+    }
+  }
+  prod: {
+    appServiceApp: {
+      alwaysOn: true
+    }
+    appServicePlan: {
+      sku: {
+        name: 'S1'
+        capacity: 2
+      }
+    }
+    xyzStorageAccount: {
+      sku: {
+        name: 'Standard_ZRS'
+      }
+    }
+  }
+}
+var xyzStorageAccountConnectionString = 'DefaultEndpointsProtocol=https;AccountName=${xyzStorageAccount.name};EndpointSuffix=${environment().suffixes.storage};AccountKey=${xyzStorageAccount.listKeys().keys[0].value}'
+
+resource appServicePlan 'Microsoft.Web/serverFarms@2020-06-01' = {
+  name: appServicePlanName
+  location: location
+  sku: environmentConfigurationMap[environmentType].appServicePlan.sku
+}
+
+resource appServiceApp 'Microsoft.Web/sites@2020-06-01' = {
+  name: appServiceAppName
+  location: location
+  properties: {
+    serverFarmId: appServicePlan.id
+    httpsOnly: true
+    siteConfig: {
+      alwaysOn: environmentConfigurationMap[environmentType].appServiceApp.alwaysOn
+      appSettings: [
+        {
+          name: 'XyzStorageAccountConnectionString'
+          value: xyzStorageAccountConnectionString
+        }
+      ]
+    }
+  }
+}
+
+resource xyzStorageAccount 'Microsoft.Storage/storageAccounts@2021-02-01' = {
+  name: xyzStorageAccountName
+  location: location
+  kind: 'StorageV2'
+  sku: environmentConfigurationMap[environmentType].xyzStorageAccount.sku
+}
+
+
+
+output appServiceAppHostName string = appServiceApp.properties.defaultHostName
---
+++ docs/scripts/tutorial-04-whole-command.ps1
@@ -0,0 +1,126 @@
+
+
+
+## Login to Azure Portal account 
+## - Note that this is interactive, and will launch a browser window to do login
+Connect-AzAccount
+
+
+
+## Configurations
+
+### GitHub
+$githubOrganisationName = 'RogerHowellDfE'
+$githubRepositoryName = 'IaC-CI-CD-Learning'
+
+###
+$branchName = 'tutorial-04'
+$tutorialSuffix = "-$($branchName)"
+
+### AD Application
+$adApplicationDisplayName = "bicep-ci-cd-learning-github-workflow$($tutorialSuffix)"
+$adApplicationName = "bicep-ci-cd-learning-github-workflow$($tutorialSuffix)"
+
+### Resource Group
+$resourceGroupName = "Xyz$($tutorialSuffix)"
+$resourceGroupLocation = 'uksouth'
+
+
+Write-Host "githubOrganisationName      : $($githubOrganisationName)"
+Write-Host "githubRepositoryName        : $($githubRepositoryName)"
+Write-Host "branchName                  : $($branchName)"
+Write-Host "tutorialSuffix              : $($tutorialSuffix)"
+Write-Host "adApplicationDisplayName    : $($adApplicationDisplayName)"
+Write-Host "adApplicationName           : $($adApplicationName)"
+Write-Host "resourceGroupName           : $($resourceGroupName)"
+Write-Host "resourceGroupLocation       : $($resourceGroupLocation)"
+
+
+## Create new resource group
+$resourceGroup = New-AzResourceGroup -Name $($resourceGroupName) -Location $($resourceGroupLocation)
+
+## Show the resource group
+Get-AzResourceGroup -Name $($resourceGroupName)
+
+
+## Create new Azure Active Directory (AD) Application, only if it doesn't already exist
+## Not explicitly required, but if check not performed then potential for `$application` to contain multiple values
+$applicationRegistration
+if(((Get-AzADApplication -DisplayName $adApplicationDisplayName) | measure).Count -gt 0) {
+    Write-Error 'Not creating new AD Application Registration - one already exists with this name'
+    ## exit 1
+} else {
+    $applicationRegistration = New-AzADApplication -DisplayName "$($adApplicationDisplayName)"
+}
+
+## Show (newly-created) application details
+## Also visible in Web UI: 
+## - https://portal.azure.com/#view/Microsoft_AAD_IAM/ActiveDirectoryMenuBlade/~/RegisteredApps
+Get-AzADApplication -DisplayName $adApplicationDisplayName
+Get-AzADApplication -OwnedApplication
+
+## Get reference to application 
+## - Note: Mostly interchangable with using `$applicationRegistration`, but `$applicationRegistration` exists only when the application is first created
+$application = Get-AzADApplication -DisplayName $adApplicationDisplayName
+
+
+## Get secrets associated with (newly-created) application
+
+Write-Host "AZURE_TENANT_ID:       $((Get-AzContext).Tenant.Id)"
+Write-Host "AZURE_SUBSCRIPTION_ID: $((Get-AzContext).Subscription.Id)"
+Write-Host "AZURE_CLIENT_ID:       $((Get-AzADApplication -DisplayName $adApplicationDisplayName).AppId)" ## Instructions state `.ApplicationId`, but actually `.AppId`
+
+
+
+## Create Azure AD Application Federated Credentials for the (newly-created) AD Application Registration to be accessed by GitHub
+## See tutorial 02 for addiitonal detail about this (short version: we're telling Azure which GitHub credentials are permitted)
+## 
+## - Below is specifically for the `$(branchName)` branch of this specific repo (also possible to apply to environments, pull requests, tags)
+##
+$federatedCredentialName_tutorialSpecificBranch = "$($adApplicationName)-github-branch-$($branchName)"
+New-AzADAppFederatedCredential `
+   -Name $federatedCredentialName_tutorialSpecificBranch`
+   -ApplicationObjectId $application.Id `
+   -Issuer 'https://token.actions.githubusercontent.com' `
+   -Audience 'api://AzureADTokenExchange' `
+   -Subject "repo:$($githubOrganisationName)/$($githubRepositoryName):ref:refs/heads/$($branchName)"
+
+
+# ## Optionally also trigger for main branch
+# $federatedCredentialName_mainBranch = "$($adApplicationName)-github-branch-main"
+# New-AzADAppFederatedCredential `
+#    -Name $federatedCredentialName_mainBranch`
+#    -ApplicationObjectId $application.Id `
+#    -Issuer 'https://token.actions.githubusercontent.com' `
+#    -Audience 'api://AzureADTokenExchange' `
+#    -Subject "repo:$($githubOrganisationName)/$($githubRepositoryName):ref:refs/heads/$($branchName)"
+
+
+## For Tutorial 03, we introduce the use of environments
+## This means we ALSO need to grant permission when running in the context of an environment
+$federatedCredentialName_environment = "$($adApplicationName)-github-environment-$($branchName)"
+New-AzADAppFederatedCredential `
+   -Name $federatedCredentialName_environment`
+   -ApplicationObjectId $application.Id `
+   -Issuer 'https://token.actions.githubusercontent.com' `
+   -Audience 'api://AzureADTokenExchange' `
+   -Subject "repo:$($githubOrganisationName)/$($githubRepositoryName):environment:$($branchName)"
+
+## See (newly-created) federated permissions here:
+## https://portal.azure.com/#view/Microsoft_AAD_RegisteredApps/ApplicationMenuBlade/~/Credentials/appId/<REDACTED-APP-ID>/isMSAApp~/false
+
+
+## Create Service Principal to the application
+New-AzADServicePrincipal -AppId $application.AppId
+
+
+## Assign workflow identity permissions to the (newly-created) resource group
+New-AzRoleAssignment `
+   -ApplicationId $($application.AppId) `
+   -RoleDefinitionName Contributor `
+   -Scope $resourceGroup.ResourceId
+
+
+## Drop the resource group
+Remove-AzResourceGroup -Name $($resourceGroupName) -Force
+
